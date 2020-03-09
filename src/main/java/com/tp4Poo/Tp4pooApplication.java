e9908de4e446f84573b2ef365be8b0878dfca7f2
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-03-09 08:27:37 -0300

    init project. add .gitignore & README.md

---
+++ src/main/java/com/tp4Poo/Tp4pooApplication.java
@@ -0,0 +1,13 @@
+package com.tp4Poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4pooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4pooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp4Poo/Tp4pooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp4Poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4pooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
